9a136ab2f109523d883c7289ba419fdc2a131785
giorgie <giorgia.salis@gmail.com>
2013-07-10 10:38:56 +0200

    push con db

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,7 +45,7 @@ class UsersController < ApplicationController
     @user=User.new(params[:user])
     if @user.save
       #redirect_to '/home'
-      flash[:success] = "Benvenuto!"
+      #flash[:success] = "Benvenuto!"
       sign_in @user
       redirect_to homepage_users_path
     else
